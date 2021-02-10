b0194b85ce6b3e27a4082c975922ffa5f69eb0a1
Scala Steward <me@scala-steward.org>
2021-02-10 21:52:57 +0100

    Update cats-core to 2.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   import Versions._
 
   //A -> Z
-  val catsCore             = Def.setting("org.typelevel"                     %%% "cats-core"             % "2.1.1")
+  val catsCore             = Def.setting("org.typelevel"                     %%% "cats-core"             % "2.4.1")
   val scalaCssCore         = Def.setting("com.github.japgolly.scalacss"      %%% "core"                  % scalaCssV)
   val scalaCssExtReact     = Def.setting("com.github.japgolly.scalacss"      %%% "ext-react"             % scalaCssV)
   val scalaCssExtScalatags = Def.setting("com.github.japgolly.scalacss"       %% "ext-scalatags"         % scalaCssV)
