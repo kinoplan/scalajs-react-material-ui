6624e26fdab04236361f7bc45a0e1d145f994c3c
Alexey Kaziev <kazievab@gmail.com>
2022-05-26 03:47:20 +0300

    Bump versions

--- project/DependenciesNPM.scala
+++ project/DependenciesNPM.scala
@@ -11,15 +11,15 @@ object DependenciesNPM {
   val emotionCore                   = "@emotion/core"                      -> "10.3.1"
   val emotionReact                  = "@emotion/react"                     -> "11.7.1"
   val emotionStyled                 = "@emotion/styled"                    -> "10.3.0"
-  val materialUiCore                = "@mui/material"                      -> "5.2.7"
-  val materialUiIcons               = "@mui/icons-material"                -> "5.2.5"
-  val materialUiLab                 = "@mui/lab"                           -> "5.0.0-alpha.63"
+  val materialUiCore                = "@mui/material"                      -> "5.8.1"
+  val materialUiIcons               = "@mui/icons-material"                -> "5.8.0"
+  val materialUiLab                 = "@mui/lab"                           -> "5.0.0-alpha.83"
   val react                         = "react"                              -> reactV
   val reactDom                      = "react-dom"                          -> reactV
   val reactSwipeableViews           = "react-swipeable-views"              -> reactSwipeableViewsV
   val reactSwipeableViewsUtils      = "react-swipeable-views-utils"        -> reactSwipeableViewsV
-  val typesReact                    = "@types/react"                       -> "17.0.38"
-  val typesReactDom                 = "@types/react-dom"                   -> "17.0.11"
+  val typesReact                    = "@types/react"                       -> "17.0.45"
+  val typesReactDom                 = "@types/react-dom"                   -> "17.0.17"
   val typesReactSwipeableViews      = "@types/react-swipeable-views"       -> "0.13.1"
   val typesReactSwipeableViewsUtils = "@types/react-swipeable-views-utils" -> "0.13.3"
 
